e5e5cd3925abd79a5cbdf970feca6a21bf077c39
princeps091-binf <princeps091@gmail.com>
2022-02-11 17:32:36 +0100

    functional stratification of the variants found across individual specimen

--- code/VCF_ICGC_explore.R
+++ code/VCF_ICGC_explore.R
@@ -40,7 +40,7 @@ breast_cancer_mutation_tbl<-breast_var_tbl %>%
 
 
 breast_cancer_mutation_tbl<-breast_var_tbl %>% 
-  filter(icgc_specimen_id %in% primary_tumor_id$icgc_specimen_id & chromosome == 22) 
+  filter(icgc_specimen_id %in% primary_tumor_id$icgc_specimen_id & chromosome == 1) 
 
 specimen_rank_tbl<-breast_cancer_mutation_tbl %>% group_by(icgc_specimen_id) %>% 
   summarise(n=n()) %>% 
@@ -48,5 +48,68 @@ specimen_rank_tbl<-breast_cancer_mutation_tbl %>% group_by(icgc_specimen_id) %>%
   arrange(rank)
 breast_cancer_mutation_tbl %>% 
   left_join(.,specimen_rank_tbl) %>% 
-  ggplot(.,aes(chromosome_start,y=rank))+geom_point(alpha=0.01,size=0.5)#+facet_grid(icgc_specimen_id ~.,scales="free")
+#  mutate(categ=ifelse(n<500,"low burden (<500 mutations)","high burden (>500 mutations)")) %>% 
+  ggplot(.,aes(chromosome_start,y=rank))+geom_point(alpha=0.02,size=0.5)#+facet_grid(categ ~.,scales="free")
 #---------------------------------------------
+# explanation of specimen metadata: https://docs.cancergenomicscloud.org/docs/icgc-metadata
+
+library(GenomicRanges)
+library(ChIPseeker)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+
+chr_var<-breast_cancer_mutation_tbl %>% distinct(chromosome,chromosome_start,chromosome_end) %>% mutate(chromosome=paste0("chr",chromosome))
+
+var_Grange<-GRanges(seqnames=chr_var$chromosome,
+                    ranges = IRanges(start=chr_var$chromosome_start,
+                                     end=chr_var$chromosome_end)
+)
+
+sample_var_fn_dist_tbl<-do.call(bind_rows,lapply(unique(breast_cancer_mutation_tbl$icgc_specimen_id),function(ID){
+  
+  sample_chr_var<-breast_cancer_mutation_tbl %>% 
+    filter(icgc_specimen_id == ID) %>% 
+    distinct(chromosome,chromosome_start,chromosome_end) %>% mutate(chromosome=paste0("chr",chromosome))
+  
+  sample_var_Grange<-GRanges(seqnames=sample_chr_var$chromosome,
+                             ranges = IRanges(start=sample_chr_var$chromosome_start,
+                                              end=sample_chr_var$chromosome_end)
+  )
+  
+  
+  peakAnno <- annotatePeak(sample_var_Grange, tssRegion=c(-3000, 3000),TxDb=txdb, annoDb="org.Hs.eg.db",verbose = F)
+  
+  return(peakAnno@annoStat %>% mutate(sample=ID))
+}))
+
+
+specimen_rank_tbl<-breast_cancer_mutation_tbl %>% group_by(icgc_specimen_id) %>% 
+  summarise(n=n()) %>% 
+  mutate(rank=dense_rank(n)) %>% 
+  arrange(rank) %>% mutate(pos=1:n())
+
+sample_var_fn_dist_tbl %>%
+  left_join(.,specimen_rank_tbl,by=c("sample"="icgc_specimen_id")) %>% 
+  left_join(.,breast_sample_info %>% dplyr::select(icgc_specimen_id,specimen_donor_treatment_type ),by=c("sample"="icgc_specimen_id")) %>% 
+  mutate(categ=ifelse(n<500,"low burden (<500 mutations)","high burden (>500 mutations)")) %>% 
+  ggplot(.,aes(pos,Frequency,fill=Feature))+scale_fill_brewer(palette="Paired")+
+  geom_bar(stat="identity")+
+  facet_grid(specimen_donor_treatment_type ~categ,scales="free")
+
+
+
+fn_repo<-"~/Documents/multires_bhicect/data/epi_data/fn_BED/"
+
+fn_folder<-paste0(fn_repo,chromo,"/")
+fn_file<-grep('BED$',list.files(fn_folder),value = T)
+fn_bed_l<-lapply(fn_file,function(f){
+  read_bed(paste0(fn_folder,f),n_fields = 3)
+})
+names(fn_bed_l)<-fn_file
+txdb_chr <- txdb
+seqlevels(txdb_chr)  <- chromo
+
+
+
+rn_annotation<-sample(peakAnno@annoStat$Feature,size = length(chr_feature_Grange),prob = peakAnno@annoStat$Frequency/100,replace = T)
--- code/envrionment_setup.R
+++ code/envrionment_setup.R
@@ -7,5 +7,5 @@ renv::install("YuLab-SMU/ggtree")
 
 renv::install("bioc::ChIPseeker")
 renv::install("bioc::GenomicRanges")
-
+renv::install("bioc::org.Hs.eg.db")
 renv::snapshot()
